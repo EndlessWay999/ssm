0761f6ecb475a2454ce6ba14828a60c228be2105
EndlessWay <63779311+EndlessWay999@users.noreply.github.com>
2023-03-06 23:50:30 +0800

    Update Test.java
    远程修改

--- src/main/java/com/endlessway/gittest/Test.java
+++ src/main/java/com/endlessway/gittest/Test.java
@@ -12,5 +12,16 @@ public class Test {
         System.out.println("hot-fix-->hello world3!");
         System.out.println("hot-fix-->hello world4!");
         System.out.println("hot-fix-->hello world5!");
+        
+         System.out.println("hello world!");
+        System.out.println("master--->hello world!");
+        System.out.println("hot-fix-->hello world!");
+        System.out.println("master-->hello world1!");
+        System.out.println("master-->hello world2!");
+        System.out.println("master-->hello world3!");
+        System.out.println("hot-fix-->hello world2!");
+        System.out.println("hot-fix-->hello world3!");
+        System.out.println("hot-fix-->hello world4!");
+        System.out.println("hot-fix-->hello world5!");
     }
 }
